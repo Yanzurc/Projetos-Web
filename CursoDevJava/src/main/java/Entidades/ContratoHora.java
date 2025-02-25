2127fcdb2b4038e4288fa2cfb15476653e5621ee
Yan Cruz <98358800+Yanzurc@users.noreply.github.com>
2025-02-25 12:53:09 -0300

    Add files via upload

---
+++ CursoDevJava/src/main/java/Entidades/ContratoHora.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entidades;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Yan Cruz
+ */
+public class ContratoHora {
+    private Date date;
+    private double valorPorHora;
+    private Integer horas;
+    
+    public ContratoHora(){
+    
+    }
+
+    public ContratoHora(Date date, double valorPorHora, Integer horas) {
+        this.date = date;
+        this.valorPorHora = valorPorHora;
+        this.horas = horas;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public double getValorPorHora() {
+        return valorPorHora;
+    }
+
+    public void setValorPorHora(double valorPorHora) {
+        this.valorPorHora = valorPorHora;
+    }
+
+    public Integer getHoras() {
+        return horas;
+    }
+
+    public void setHoras(Integer horas) {
+        this.horas = horas;
+    }
+    
+    
+}
